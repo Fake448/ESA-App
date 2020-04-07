dd92c1c113a1f7cfea3bb0f8f0ae53681aa818d3
Fake <drdolittle448@yahoo.de>
2020-04-07 20:08:31 +0200

    rebuilding index-handler

--- public/electron.js
+++ public/electron.js
@@ -5,127 +5,173 @@ const fs = require("fs");
 
 let MainWindow;
 let ArtikelWindow;
-let Datenbase;
+var DB = [];
 let currentID;
 
 //  Setup     DEVELOP - Environment
-const devPath = "http://localhost:3000";
-function prodpath (src) {
-	const target = "../build/index.html?" + src
-	console.log("Target:");
-	console.log(target);
-	
-	// `file://${path.join(__dirname, "../build/index.html?viewMain")}`
+const devPath = "http://localhost:3000?";
 
-	return target
-}
+function prodPath(myView) {
+   const target = "../build/index.html?" + myView;
 
+   // `file://${path.join(__dirname, "../build/index.html?viewMain")}`
 
-
-
-function getDataBase() {
-	fs.readFile(__dirname + "/data/database.json", function(err, data) {
-		if (err) throw err;
-		data = JSON.parse(data.toString());
-		return data;
-	});
+   return target;
 }
 
 //  creating MAIN-Window
-
 function createMainWindow() {
-	Datenbase = getDataBase();
-	console.log("test");
-	
-	MainWindow = new BrowserWindow({
-		width: 600,
-		height: 1000,
-		webPreferences: {
-			nodeIntegration: true,
-		},
-	});
+   MainWindow = new BrowserWindow({
+      width: 800,
+      height: 1000,
+      webPreferences: {
+         nodeIntegration: true,
+      },
+   });
 
-	MainWindow.loadURL(
-		isDev
-			? devPath + "?viewMain"
-			: `file://${path.join(__dirname, "../build/index.html?viewMain")}`
-	);
+   MainWindow.loadURL(isDev ? devPath + "viewMain" : prodPath("viewMain"));
 
-	MainWindow.webContents.openDevTools();
-	const {
-		default: installExtension,
-		REACT_DEVELOPER_TOOLS,
-	} = require("electron-devtools-installer");
+   // 	DEV 	Extras
+   MainWindow.webContents.openDevTools();
+   const {
+      default: installExtension,
+      REACT_DEVELOPER_TOOLS,
+   } = require("electron-devtools-installer");
 
-	installExtension(REACT_DEVELOPER_TOOLS)
-		.then((name) => {
-			console.log("added Extension: REACT_DEVELOPER_TOOLS");
-		})
-		.catch((err) => {
-			console.log(err);
-		});
+   installExtension(REACT_DEVELOPER_TOOLS)
+      .then((name) => {
+         console.log("added Extension: REACT_DEVELOPER_TOOLS");
+      })
+      .catch((err) => {
+         console.log(err);
+      });
 }
 
 ipcMain.on("mainWindow:loaded", () => {
-	fs.readFile(__dirname + "/data/database.json", function(err, data) {
-		if (err) throw err;
-		const datenbase = JSON.parse(data.toString());
-		let mydata = [];
-		datenbase["Parts"].forEach((mypart, i) => {
-			mydata.push({
-				name: mypart["Name"] + " " + mypart["ArtNumber"],
-				id: i,
-			});
-		});
-		MainWindow.webContents.send("receiveArtikelData", mydata);
-	});
+   const DB = getDataBase();
+   const TmpDB = [
+      {
+         name: "abc",
+         id: 1,
+      },
+      {
+         name: "fdfsd",
+         id: 2,
+      },
+   ];
+
+   fs.readFile(__dirname + "/data/database.json", function(err, data) {
+      if (err) throw err;
+      const database = JSON.parse(data.toString());
+      let mydata = [];
+      database["Parts"].forEach((mypart, i) => {
+         mydata.push({
+            name: mypart["Name"] + " " + mypart["ArtNumber"],
+            id: i,
+         });
+      });
+      // console.log(mydata);
+
+      MainWindow.webContents.send("receiveArtikelData", TmpDB);
+   });
 });
 
-//  creating ARTIKEL-Window
-
-function createArtikelWindow() {
-	ArtikelWindow = new BrowserWindow({
-		width: 800,
-		height: 600,
-		webPreferences: {
-			nodeIntegration: true,
-		},
-	});
-	ArtikelWindow.loadURL(
-		isDev
-			? devPath + "?viewArtikel"
-			: `file://${path.join(__dirname, "../build/index.html?viewArtikel")}`
-	);
-}
-
 ipcMain.on("ArtikelWindow:loaded", () => {
-	fs.readFile(__dirname + "/database.json", function(err, data) {
-		if (err) throw err;
-		const studentInfo = JSON.parse(data.toString());
-		let part = studentInfo["Parts"][currentID];
-		ArtikelWindow.webContents.send("receiveStudentInfo", part);
-	});
+   // DB = getmyDB();
+   // console.log(DB);
+
+   // fs.readFile(__dirname + "/database.json", function(err, data) {
+      // if (err) throw err;
+      // const studentInfo = JSON.parse(data.toString());
+      // let part = studentInfo["Parts"][currentID];
+      // ArtikelWindow.webContents.send("receiveStudentInfo", part);
+   // });
 });
 
 ipcMain.on("part:clicked", (e, ID) => {
-	currentID = ID;
-	createArtikelWindow();
+   currentID = ID;
+   createArtikelWindow();
 });
 
-// SETTINGS Electron-App
+//  creating ARTIKEL-Window
+function createArtikelWindow() {
+   ArtikelWindow = new BrowserWindow({
+      width: 800,
+      height: 600,
+      webPreferences: {
+         nodeIntegration: true,
+      },
+   });
+   ArtikelWindow.loadURL(
+      isDev ? devPath + "viewArtikel" : prodPath("viewArtikel")
+   );
+}
 
+// SETTINGS Electron-App
 app.whenReady().then(createMainWindow);
 
 app.on("window-all-closed", () => {
-	if (process.platform !== "darwin") {
-		app.quit();
-	}
+   if (process.platform !== "darwin") {
+      app.quit();
+   }
 });
 
 app.on("activate", () => {
-	if (BrowserWindow.getAllWindows().length === 0) {
-		createMainWindow();
-	}
+   if (BrowserWindow.getAllWindows().length === 0) {
+      createMainWindow();
+   }
 });
 
-app.on("ready", () => {});
+app.on("ready", () => {
+   console.log("ESA-Tool started");
+});
+
+//		HELPER functions
+
+const MyDB = getmyDB();
+
+function getmyDB() {
+   const TmpDB = [
+      {
+         name: "abc",
+         id: 1,
+      },
+      {
+         name: "fdfsd",
+         id: 2,
+      },
+   ];
+
+   return TmpDB;
+}
+
+function getDataBase() {
+   // Dies gibt alle Dateien die sich in Ihrem Root-Verzeichnis,
+   // befinden aus, '/' oder 'C:\'.
+   // const root = fs.readdirSync('/Games')
+   // console.log(root)
+
+   const files = require("fs");
+
+   var myDB = [];
+	
+   files.readFile(__dirname + "/data/database.json", function(err, data) {
+		if (err) throw err;
+      const database = JSON.parse(data.toString());
+      database["Parts"].forEach((mypart, i) => {
+			myDB.push({
+				name: mypart["Name"],
+            ArtNr: mypart["ArtNumber"],
+            id: i,
+         });
+      });
+		this.DB = myDB
+		
+   });
+	
+	// console.log(this.DB);
+	// console.log(DB);
+	
+	console.log(myDB);
+   return myDB;
+}
