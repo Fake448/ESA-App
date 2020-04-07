c1c7f9d4aa682e98b4095e4fe608dd3de707f473
Fake <drdolittle448@yahoo.de>
2020-04-07 20:09:33 +0200

    -beautify code-

--- src/components/Content.js
+++ src/components/Content.js
@@ -2,48 +2,51 @@ import React from "react";
 const { ipcRenderer } = window.require("electron");
 
 export default class Content extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      test: 12345,
-    };
-    ipcRenderer.on("receiveArtikelData", (e, parts) => {
-      this.setState({
-        parts: parts,
+   constructor(props) {
+      super(props);
+      this.state = {
+         test: 12345,
+      };
+      ipcRenderer.on("receiveArtikelData", (e, parts) => {
+         this.setState({
+            parts: parts,
+         });
       });
-    });
-  }
+   }
 
-  handleClick = (e) => {
-    ipcRenderer.send("part:clicked", e.currentTarget.id);
-  };
+   handleClick = (e) => {
+      ipcRenderer.send("part:clicked", e.currentTarget.id);
+   };
 
-  render() {
-    return (
-      <React.Fragment>
-        <div className="content">
-          <h4> Parts: </h4>
-          <ul className="parts">
-            {this.state.parts
-              ? this.state.parts.map((part) => (
-                  <li key={part["name"]}>
-                    <button onClick={this.handleClick} id={part["name"]}>
-                      View
-                    </button>
-                    <p
-                      style={{
-                        paddingLeft: "10px",
-                        display: "inline-block",
-                      }}
-                    >
-                      {part["name"]}
-                    </p>
-                  </li>
-                ))
-              : null}
-          </ul>
-        </div>
-      </React.Fragment>
-    );
-  }
+   render() {
+      return (
+         <React.Fragment>
+            <div className="content">
+               <h4> Parts: </h4>
+               <ul className="parts">
+                  {this.state.parts
+                     ? this.state.parts.map((part) => (
+                          <li key={part["name"]}>
+                             <button
+                                onClick={this.handleClick}
+                                id={part["name"]}
+                             >
+                                View
+                             </button>
+                             <p
+                                style={{
+                                   paddingLeft: "10px",
+                                   display: "inline-block",
+                                }}
+                             >
+                                {part["name"]}
+                             </p>
+                          </li>
+                       ))
+                     : null}
+               </ul>
+            </div>
+         </React.Fragment>
+      );
+   }
 }
