c4a3843020cc2001673d46d01c68e84de822449b
Fake <drdolittle448@yahoo.de>
2020-04-17 01:39:47 +0200

    prettier

--- src/renderer/windows/main/main.jsx
+++ src/renderer/windows/main/main.jsx
@@ -1,22 +1,21 @@
 import React from "react";
 import ToolBar from "../../components/Toolbar/ToolBar";
 import SideBar from "../../components/Sidebar/SideBar";
-import Home from '../../components/Content/Home/Home'
-import GNS from '../../components/Content/GNS/GNS'
-import GIV from '../../components/Content/GIV/GIV'
-import Elektrant from '../../components/Content/Elektrant/Elektrant'
+import Home from "../../components/Content/Home/Home";
+import GNS from "../../components/Content/GNS/GNS";
+import GIV from "../../components/Content/GIV/GIV";
+import Elektrant from "../../components/Content/Elektrant/Elektrant";
 import Content from "../../components/Content/Content";
 import "./main.css";
 const { ipcRenderer } = window.require("electron");
-
 
 class mainWindow extends React.Component {
    constructor(props) {
       super(props);
 
-      this.state =  {
-         newTest: '',
-         content : [
+      this.state = {
+         newTest: "",
+         content: [
             {
                name: "Home",
                component: <Home />
@@ -34,27 +33,25 @@ class mainWindow extends React.Component {
                component: <Elektrant />
             }
          ]
-      }
-         
+      };
+
       ipcRenderer.on("receiveArtikelData", (e, artikel) => {
          this.setState({ artikel: artikel });
-         
       });
    }
-   
+
    componentDidMount() {
       ipcRenderer.send("mainWindow:loaded");
       console.log(this.state);
    }
 
    formSubmitted(event) {
-      event.preventDefault()
+      event.preventDefault();
       console.log(event);
    }
 
    suchechanged(event) {
       console.log(event.target.value);
-      
    }
 
    render() {
@@ -63,9 +60,13 @@ class mainWindow extends React.Component {
             <ToolBar />
             <div className="content_wrapper">
                <SideBar />
-               <Content className="content" component={this.state.content}/>
+               <Content className="content" component={this.state.content} />
                <form onSubmit={this.formSubmitted}>
-                  <input onChange={this.suchechanged} id="sucheInput" name="sucheInput"></input>
+                  <input
+                     onChange={this.suchechanged}
+                     id="sucheInput"
+                     name="sucheInput"
+                  ></input>
                   <button type="submit">suche</button>
                </form>
                {/* <GNS /> */}
