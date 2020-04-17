54da66b8f39b625833c0d74c7e431a27cdf8d774
Fake <drdolittle448@yahoo.de>
2020-04-17 03:06:07 +0200

    prettier

--- src/index.js
+++ src/index.js
@@ -3,5 +3,4 @@ import ReactDOM from "react-dom";
 import ViewManager from "./renderer/windows/ViewManager";
 import "./index.css";
 
-
 ReactDOM.render(<ViewManager />, document.getElementById("root"));
