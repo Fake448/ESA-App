41fb91b368f8ab72044230a38e7485cf189d6411
Fake <drdolittle448@yahoo.de>
2020-04-17 03:57:08 +0200

    getting logo from /public

--- src/renderer/components/Toolbar/ToolBar.js
+++ src/renderer/components/Toolbar/ToolBar.js
@@ -1,25 +1,32 @@
 import React from "react";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 import "./ToolBar.css";
-
 
 class Toolbar extends React.Component {
    render() {
       return (
          <React.Fragment>
-            <div className="toolbar">
-               <div></div>
-               <div className="toolbar_logo"></div>
-                  <ul className="toolbar_list">
-                     <li>
-                        <Link to='/index.html?viewMain'>Home</Link>
-                     </li>
-                     <li>
-                        <Link to='/index.html?viewArtikel'>Artikel</Link>
-                     </li>
-                     <li>About</li>
-                  </ul>
+            <div className="navbar nav_title">
+               <div className="toolbar_logo">
+                  <img
+                     src={process.env.PUBLIC_URL + "/img/logo/icon.png"}
+                     alt="error"
+                     style={{ width: 100 }}
+                  ></img>
                </div>
+               {/* <div className="profile">
+                  <div className="profile_pic"></div>
+               </div> */}
+               <ul className="toolbar_list">
+                  <li>
+                     <Link to="/index.html?viewMain">Home</Link>
+                  </li>
+                  <li>
+                     <Link to="/index.html?viewArtikel">Artikel</Link>
+                  </li>
+                  <li>About</li>
+               </ul>
+            </div>
          </React.Fragment>
       );
    }
