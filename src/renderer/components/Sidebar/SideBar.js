3e88445c0e4581c3edcfb4570c2443a6150c178c
Fake <drdolittle448@yahoo.de>
2020-04-19 18:23:57 +0200

    links generated via props from main added logo (svg)

--- src/renderer/components/Sidebar/SideBar.js
+++ src/renderer/components/Sidebar/SideBar.js
@@ -1,45 +1,42 @@
 import React from "react";
-import './SideBar.css'
-
-const navLinks = [
-   {
-      text : 'home',
-      path : '/',
-      icon : ''
-   },
-   {
-      text : 'GIV',
-      path : '/GIV',
-      icon : ''
-   },
-   {
-      text : 'GNS',
-      path : '/GNS',
-      icon : ''
-   },
-   {
-      text : 'Elektrant',
-      path : '/Elektrant',
-      icon : ''
-   },
-]
-
-
-
+import "./SideBar.css";
+import { AccessAlarm, ThreeDRotation } from "@material-ui/icons";
 
 export default class Sidebar extends React.Component {
+   constructor(props) {
+      super(props);
+      console.log("PROPS", props);
 
-
+      // this.setState(props)
+      this.state = {
+         //    props : (props)
+      };
+   }
 
    render() {
       return (
          <React.Fragment>
             <div className="sidebar">
-               <ul>
-                  <li>Home</li>
-                  <li>GIV</li>
-                  <li>GNS</li>
-                  <li>Elektrant</li>
+               <div className="sidebar_logo">
+                  <img
+                     src={process.env.PUBLIC_URL + "/img/logo/icon.svg"}
+                     alt="error"
+                  ></img>
+               </div>
+
+               <ul className="sidebar_navlist">
+                  {this.props.links
+                     ? this.props.links.map(link => (
+                          <li key={link["name"]}>
+                             <button key={link.name}>
+                                {link.name}
+                                <span className="badge badge-secondary">
+                                   New
+                                </span>
+                             </button>
+                          </li>
+                       ))
+                     : null}
                </ul>
             </div>
          </React.Fragment>
