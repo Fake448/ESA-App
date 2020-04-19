0ae8d8a006f9fb3587f117973388d5b8ca318d72
Fake <drdolittle448@yahoo.de>
2020-04-19 18:24:52 +0200

    updating data sending navlinks to SideBar

--- src/renderer/windows/main/main.jsx
+++ src/renderer/windows/main/main.jsx
@@ -4,8 +4,8 @@ import SideBar from "../../components/Sidebar/SideBar";
 import Home from "../../components/Content/Home/Home";
 import GNS from "../../components/Content/GNS/GNS";
 import GIV from "../../components/Content/GIV/GIV";
+import Test from "../../components/Content/Test/Dashboard";
 import Elektrant from "../../components/Content/Elektrant/Elektrant";
-import Content from "../../components/Content/Content";
 import "./main.css";
 const { ipcRenderer } = window.require("electron");
 
@@ -14,23 +14,36 @@ class mainWindow extends React.Component {
       super(props);
 
       this.state = {
-         newTest: "",
-         content: [
+         mainContent: [
             {
                name: "Home",
-               component: <Home />
-            },
-            {
-               name: "GIV",
-               component: <GIV />
+               component: <Home />,
+               isActive: true
             },
             {
                name: "GNS",
-               component: <GNS />
+               component: <GNS />,
+               isActive: false
+            },
+            {
+               name: "GIV",
+               component: <GIV />,
+               isActive: false
             },
             {
                name: "Elektrant",
-               component: <Elektrant />
+               component: <Elektrant />,
+               isActive: false
+            },
+            {
+               name: "Automobile",
+               // component: <Automobile />,
+               isActive: false
+            },
+            {
+               name: "Test",
+               component: <Test />,
+               isActive: false
             }
          ]
       };
@@ -42,7 +55,6 @@ class mainWindow extends React.Component {
 
    componentDidMount() {
       ipcRenderer.send("mainWindow:loaded");
-      console.log(this.state);
    }
 
    formSubmitted(event) {
@@ -59,18 +71,33 @@ class mainWindow extends React.Component {
          <React.Fragment>
             <ToolBar />
             <div className="content_wrapper">
-               <SideBar />
-               <Content className="content" component={this.state.content} />
-               <form onSubmit={this.formSubmitted}>
-                  <input
-                     onChange={this.suchechanged}
-                     id="sucheInput"
-                     name="sucheInput"
-                  ></input>
-                  <button type="submit">suche</button>
-               </form>
-               {/* <GNS /> */}
-               {/* <GIV /> */}
+               <SideBar links={this.state.mainContent} />
+
+               <div className="content">
+                  {this.state.mainContent.map(content => (
+                     <div
+                        key={content["name"]}
+                        id={content["name"]}
+                        // isActive={content["isActive"]}
+                     >
+                        {content.component}
+                     </div>
+                  ))}
+
+                  {/* <Content
+                  className="content"
+                  component={this.state.mainContent}
+               /> */}
+
+                  <form onSubmit={this.formSubmitted}>
+                     <input
+                        onChange={this.suchechanged}
+                        id="sucheInput"
+                        name="sucheInput"
+                     ></input>
+                     <button type="submit">suche</button>
+                  </form>
+               </div>
             </div>
          </React.Fragment>
       );
