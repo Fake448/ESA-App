d48e0e102db88480f9940be93b01db0a86810552
Fake <drdolittle448@yahoo.de>
2020-04-19 18:20:44 +0200

    more data for Elektrant

--- src/renderer/components/Content/Elektrant/Elektrant.js
+++ src/renderer/components/Content/Elektrant/Elektrant.js
@@ -1,19 +1,64 @@
 import React from "react";
-const { ipcRenderer } = window.require("electron");
+import ListSeriesParameter from '../utils/ListSeriesParameter'
+// const { ipcRenderer } = window.require("electron");
 
 class Elektrant extends React.Component {
    state = {};
    constructor(props) {
       super(props);
-      console.log("Elektrant-props", props);
-      console.log("Elektrant-state", this.state);
+      this.state = {
+         infos: [
+            {
+               name: "Energieanschlusssäulen",
+               symbol: "ELK",
+               bauform: [
+                  {
+                     name: "Elektrant AS",
+                     symbol: "AS",
+                     variante: [
+                        {
+                           name: "AS-11"
+                        },
+                        {
+                           name: "AS-21"
+                        },
+                        {
+                           name: "AS-31"
+                        }
+                     ]
+                  },
+                  {
+                     name: "Elektrant ES",
+                     symbol: "AS",
+                     variante: [
+                        {
+                           name: "ES-11"
+                        },
+                        {
+                           name: "ES-21"
+                        },
+                        {
+                           name: "ES-31"
+                        }
+                     ]
+                  }
+               ]
+            },
+            {
+               name: "Zähler- Wandler- und Energieverteilungen bis 1000 A",
+               symbol: "ELK"
+            }
+         ]
       };
-   
+   }
+
    render() {
       return (
          <React.Fragment>
             <div className="content">
                <h1>Elektrant</h1>
+               <ListSeriesParameter infos={this.state.infos} />
+
             </div>
          </React.Fragment>
       );
