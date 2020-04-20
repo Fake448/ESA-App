55da7f2bd617adf87cfd17909fb17ded0fb416bf
Fake <drdolittle448@yahoo.de>
2020-04-20 20:13:32 +0200

    adding "under construction" pmg

--- src/renderer/windows/artikel/artikel.jsx
+++ src/renderer/windows/artikel/artikel.jsx
@@ -1,27 +1,35 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import page404 from "../../../";
+const { ipcRenderer } = window.require("electron");
 
-const { ipcRenderer } = window.require('electron');
+console.log(process.cwd());
 
-class artikelWindow extends Component{
+class artikelWindow extends Component {
+   constructor(props) {
+      super(props);
 
-    constructor(props){
-        super(props);
-        
-        ipcRenderer.on('sendingArtikelinfo', (e, part) => {
-            this.setState({part: part})
-        });
-    }
+      ipcRenderer.on("sendingArtikelinfo", (e, part) => {
+         this.setState({ part: part });
+      });
+   }
 
-    componentDidMount(){
-        ipcRenderer.send('artikelWindow:loaded');
-    }
+   componentDidMount() {
+      ipcRenderer.send("artikelWindow:loaded");
+   }
 
-    render(){
-        console.log("props: ", this.props, this.state)
-        return(
+   render() {
+      console.log("props: ", this.props, this.state);
+      return (
+         <React.Fragment>
             <h1>Artikel-Fenster</h1>
-        );
-    }
+            <img
+               src={process.env.PUBLIC_URL + "/img/404.png"}
+               alt="error"
+               width="100%"
+            ></img>
+         </React.Fragment>
+      );
+   }
 }
 
 export default artikelWindow;
