bbfa3d1ceb905b1504d933723b386ec409094abf
Fake <drdolittle448@yahoo.de>
2020-04-21 00:07:44 +0200

    testing with inline function

--- src/renderer/windows/main/main.jsx
+++ src/renderer/windows/main/main.jsx
@@ -10,136 +10,132 @@ import "./main.css";
 const { ipcRenderer } = window.require("electron");
 
 class mainWindow extends React.Component {
-   constructor(props) {
-      super(props);
+  constructor(props) {
+    super(props);
 
-      this.state = {
-         mainContent: [
-            {
-               name: "Home",
-               component: <Home />,
-               isActive: true
-            },
-            {
-               name: "ATM - Automobile",
-               // component: <Automobile />,
-               isActive: false
-            },
-            {
-               name: "BAN - Bahn",
-               // component: <Automobile />,
-               isActive: false
-            },
-            {
-               name: "ELK - Elektrant",
-               component: <Elektrant />,
-               isActive: false
-            },
-            {
-               name: "GNS",
-               component: <GNS />,
-               isActive: false
-            },
-            {
-               name: "GIV",
-               component: <GIV />,
-               isActive: false
-            },
-            {
-               name: "MS - Mittelspannung",
-               // component: <Automobile />,
-               isActive: false
-            },
-            {
-               name: "RIT - Rittal",
-               // component: <Automobile />,
-               isActive: false
-            },
-            {
-               name: "SIV - Sivacon",
-               // component: <Automobile />,
-               isActive: false
-            },
-            {
-               name: "SNV",
-               // component: <Automobile />,
-               isActive: false
-            },
-            {
-               name: "TAB - Tableau",
-               // component: <Automobile />,
-               isActive: false
-            },
-            {
-               name: "TRS - Trafoschrank",
-               // component: <Automobile />,
-               isActive: false
-            },
-            {
-               name: "TRL - TriLine",
-               // component: <Automobile />,
-               isActive: false
-            },
-            {
-               name: "Test",
-               // component: <Test />,
-               isActive: false
-            }
-         ]
-      };
+    this.state = {
+      mainContent: [
+        {
+          name: "Home",
+          component: <Home />,
+          isActive: true,
+        },
+        {
+          name: "ATM - Automobile",
+          // component: <Automobile />,
+          isActive: false,
+        },
+        {
+          name: "BAN - Bahn",
+          // component: <Automobile />,
+          isActive: false,
+        },
+        {
+          name: "ELK - Elektrant",
+          component: <Elektrant />,
+          isActive: false,
+        },
+        {
+          name: "GNS",
+          component: <GNS />,
+          isActive: false,
+        },
+        {
+          name: "GIV",
+          component: <GIV />,
+          isActive: false,
+        },
+        {
+          name: "MS - Mittelspannung",
+          // component: <Automobile />,
+          isActive: false,
+        },
+        {
+          name: "RIT - Rittal",
+          // component: <Automobile />,
+          isActive: false,
+        },
+        {
+          name: "SIV - Sivacon",
+          // component: <Automobile />,
+          isActive: false,
+        },
+        {
+          name: "SNV",
+          // component: <Automobile />,
+          isActive: false,
+        },
+        {
+          name: "TAB - Tableau",
+          // component: <Automobile />,
+          isActive: false,
+        },
+        {
+          name: "TRS - Trafoschrank",
+          // component: <Automobile />,
+          isActive: false,
+        },
+        {
+          name: "TRL - TriLine",
+          // component: <Automobile />,
+          isActive: false,
+        },
+        {
+          name: "Test",
+          // component: <Test />,
+          isActive: false,
+        },
+      ],
+    };
 
-      ipcRenderer.on("receiveArtikelData", (e, artikel) => {
-         this.setState({ artikel: artikel });
-      });
-   }
+    ipcRenderer.on("receiveArtikelData", (e, artikel) => {
+      this.setState({ artikel: artikel });
+    });
+  }
 
-   componentDidMount() {
-      ipcRenderer.send("mainWindow:loaded");
-   }
+  componentDidMount() {
+    ipcRenderer.send("mainWindow:loaded");
+  }
 
-   formSubmitted(event) {
-      event.preventDefault();
-      console.log(event);
-   }
+  formSubmitted(event) {
+    event.preventDefault();
+    console.log(event);
+  }
 
-   suchechanged(event) {
-      event.preventDefault();
-   }
+  suchechanged(event) {
+    event.preventDefault();
+  }
 
-   render() {
-      return (
-         <React.Fragment>
-            <ToolBar />
-            <div className="content_wrapper">
-               <SideBar links={this.state.mainContent} />
+  createContent() {
+    return this.state.mainContent.map((content) => (
+      <div key={content["name"]}>
+        {content["name"]}
+        {content.component}
+      </div>
+    ));
+  }
 
-               <div className="content">
-                  <div>
-                     {this.state.mainContent.map(content => (
-                        <div key={content["name"]}>
-                           {content["name"]}
-                           {content["isActive"]}
-                        </div>
-                     ))}
-                  </div>
-                  {this.state.mainContent.map(content => (
-                     <div key={content["name"]} id={content["name"]}>
-                        {content.component}
-                     </div>
-                  ))}
-                  <form onSubmit={this.formSubmitted}>
-                     <input
-                        onChange={this.suchechanged}
-                        id="sucheInput"
-                        name="sucheInput"
-                     ></input>
-                     <button type="submit">suche</button>
-                  </form>
-               </div>
-            </div>
-         </React.Fragment>
-      );
-   }
+  render() {
+    return (
+      <React.Fragment>
+        <ToolBar />
+        <div className="content_wrapper">
+          <SideBar links={this.state.mainContent} />
+          <div className="content">
+            {this.createContent()}
+            <form onSubmit={this.formSubmitted}>
+              <input
+                onChange={this.suchechanged}
+                id="sucheInput"
+                name="sucheInput"
+              ></input>
+              <button type="submit">suche</button>
+            </form>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default mainWindow;
