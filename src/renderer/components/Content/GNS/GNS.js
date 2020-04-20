234b9449e1cb7921ba178c43262d7c58facb7c80
Kunze <kunzechristoph@esa-grimma.local>
2020-04-20 15:48:59 +0200

    adiing more info´s + pic

--- src/renderer/components/Content/GNS/GNS.js
+++ src/renderer/components/Content/GNS/GNS.js
@@ -32,6 +32,68 @@ class GNS extends React.Component {
                name: "Blindleistungskompensation",
                symbol: "MCC"
             }
+         ],
+         infos: [
+            {
+               name: "Bemessungsbetriebsspannung Ue",
+               value: "AC 690 V"
+            },
+            {
+               name: "Bemessungsfrequenz",
+               value: "bis 60 Hz"
+            },
+            {
+               name: "Bemessungsstoßspannungsfestigkeit",
+               value: "8 kV"
+            },
+            {
+               name: "Bemessungsisolationsspannung Ui",
+               value: "1 kV"
+            },
+            {
+               name: "Überspannungskategorie",
+               value: "III"
+            },
+            {
+               name: "Verschmutzungsgrad",
+               value: "3"
+            },
+            {
+               name: "Schutzgrad nach IEC 60529",
+               value: "max. IP41"
+            },
+            {
+               name: "Schutzklasse",
+               value: "I"
+            },
+            {
+               name: "Innere Unterteilung",
+               value: "Gerüstrahmen – Alu-Zink beschichtet Innere Unterteilung – Alu-Zink beschichtet Umhüllung – Pulverbeschichtet RAL 7035 (Standard)"
+            },
+            {
+               name: "Rastergrundmaß",
+               value: "25 mm = 1E nach DIN43660"
+            },
+            {
+               name: "",
+               value: ""
+            },
+            {
+               name: "",
+               value: ""
+            },
+            {
+               name: "",
+               value: ""
+            },
+            {
+               name: "",
+               value: ""
+            },
+            {
+               name: "",
+               value: ""
+            },
          ]
       };
 
@@ -50,6 +112,11 @@ class GNS extends React.Component {
          <React.Fragment>
             <div className="content">
                <h1>GNS (ver. 5.1)</h1>
+               <img
+                     src={process.env.PUBLIC_URL + "/img/GNS/GNS-komplett.png"}
+                     alt="error"
+                     width="400"
+                  ></img>
                <h4> Parts: </h4>
                <ul className="parts">
                   {this.state.parts
