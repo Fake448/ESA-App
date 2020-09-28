332827426b912e0f1f2dc2e2f78d68041f6063b0
Kunze <kunzechristoph@esa-grimma.local>
2020-09-28 15:51:39 +0200

    Typo

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// Das eist eine der Möglichkeiten, welche ich gefunden habe,
+// Das ist eine der Möglichkeiten, welche ich gefunden habe,
 // um die jeweiligen 'window'-views anzuzeigen
 //   (Ziel war es jedes Fenster mit SEINER "start.js" zu hinterlegen)
 //       ViewManager.js als Handler
