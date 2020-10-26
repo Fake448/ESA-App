be24780bc2d13d04dc8b3fbfcabdf3545ad07d5d
Fake <drdolittle448@yahoo.de>
2020-10-26 07:00:00 +0100

    adding class

--- public/dropdown.js
+++ public/dropdown.js
@@ -1,170 +1,104 @@
 // https://www.youtube.com/watch?v=QFW2o1GA60k
 
-const data = {
-  System: [
-    {
-      Name: "Grimmaer Niederspannung 3.1",
-      Abk: "G31",
-      default: false,
-    },
-    {
-      Name: "Grimmaer Niederspannung 5.1",
-      Abk: "G51",
-      default: true,
-    },
-  ],
-  Produktebene: [
-    {
-      Name: "Typfeld",
-      Abk: "TF",
-      default: true,
-    },
-    {
-      Name: "Kalkulationsfeld",
-      Abk: "KF",
-      default: false,
-    },
-  ],
-  Feldtyp: [
-    {
-      Name: "Leistungsschalter",
-      Abk: "LS",
-      default: true,
-      Baureihe: [
-        {
-          Name: "Emax2 E1.2",
-          Abk: "E12",
-          default: true,
-        },
-        {
-          Name: "Emax2 E2.2",
-          Abk: "E22",
-          default: false,
-        },
-        {
-          Name: "Emax2 E4.2",
-          Abk: "E42",
-          default: false,
-        },
-        {
-          Name: "Emax2 E6.2",
-          Abk: "E62",
-          default: false,
-        },
-        {
-          Name: "Tmax T4",
-          Abk: "T4",
-          default: false,
-        },
-        {
-          Name: "Tmax T5",
-          Abk: "T5",
-          default: false,
-        },
-        {
-          Name: "Tmax T6",
-          Abk: "T6",
-          default: false,
-        },
-        {
-          Name: "Tmax T7",
-          Abk: "T7",
-          default: false,
-        },
-      ],
-    },
-    {
-      Name: "Sprungschaltleisten",
-      Abk: "SLw",
-      default: false,
+// fetching Data
+// - https://dmitripavlutin.com/javascript-fetch-async-await/
 
-      Baureihe: [
-        {
-          Name: "ABB",
-          Abk: "ABB",
-          default: true,
-        },
-        {
-          Name: "Jean-Müller",
-          Abk: "JM",
-          default: false,
-        },
-      ],
-    },
-    {
-      Name: "Sprungschaltleisten, senkrecht",
-      Abk: "SLs",
-      default: false,
-    },
-    {
-      Name: "Modulplatten",
-      Abk: "MP",
-      default: false,
-    },
-    {
-      Name: "Lastschaltleisten",
-      Abk: "LL",
-      default: false,
-    },
-    {
-      Name: "Motor-Control-Center",
-      Abk: "MC",
-      default: false,
-    },
-    {
-      Name: "Gebaudeinstallationsverteiler",
-      Abk: "IV",
-      default: false,
-    },
-    {
-      Name: "Eckfeld",
-      Abk: "EF",
-      default: false,
-    },
-    {
-      Name: "Kompensationsfeld",
-      Abk: "KO",
-      default: false,
-    },
-    {
-      Name: "Leerfeld",
-      Abk: "LF",
-      default: false,
-    },
-  ],
-};
+class dropdown {
+   constructor(elementID) {
+      this.data = this.getData();
+      this.element = elementID;
+   }
 
-var mytable = document.createElement("table");
-var table_body = document.createElement("tbody");
+   init() {
+      this.createTable();
+   }
 
-console.log("data :", data);
+   // readData();
+   getData() {
+      fetch("./data.json")
+         .then((response) => response.json())
+         .then((jsondata) => {
+            this.data = jsondata;
+            console.log("getData :", this.data);
+         });
+   }
 
-for (var key in data) {
-  var tr = document.createElement("tr");
-  var td_name = document.createElement("td");
-  var td_dropdown = document.createElement("td");
-  var select = document.createElement("select");
+   createTable() {
+      var table = document.createElement("table");
+      var tbody = document.createElement("tbody");
 
-  // System
-  td_name.innerHTML = key;
-  select.id = key + "_select";
+      // adding System
+      this.addRow(tbody, "System");
+      this.addRow(tbody, "sfdsd");
 
-  mytable.append(table_body);
-  table_body.append(tr);
-  tr.append(td_name);
-  tr.append(td_dropdown);
-  td_dropdown.append(select);
+      console.log("here", this.data);
 
-  for (let i = 0; i < data.System.length; i++) {
-    var option = document.createElement("option");
-    option.value = data.System[i].Abk;
-    option.innerHTML = data.System[i].Name;
+      table.appendChild(tbody);
+      document.getElementById(this.element).appendChild(table);
+   }
 
-    console.log(data.System[i].Abk);
-    console.log(data.System[i].Name);
-    select.appendChild(option);
-  }
+   addRow(table_body, name) {
+      var tableRow = document.createElement("tr");
+      var td_name = document.createElement("td");
+      var td_dropdown = document.createElement("td");
 
+      // rowName
+      td_name.innerHTML = name;
+
+      // rowDropdown
+      this.addDropdown(td_dropdown, name);
+
+      tableRow.appendChild(td_name);
+      tableRow.appendChild(td_dropdown);
+      table_body.appendChild(tableRow);
+   }
+
+   addDropdown(element, name) {
+      var select = document.createElement("select");
+      element.appendChild(select);
+   }
+
+   test() {
+      console.log("test");
+   }
+
+   //  startdropdown(data) {
+   //     console.log("dsfsdgksdglk");
+   //     console.log(data);
+   //     var mytable = document.createElement("table");
+   //     var table_body = document.createElement("tbody");
+
+   //     for (var key in data) {
+   //        var tr = document.createElement("tr");
+   //        var td_name = document.createElement("td");
+   //        var td_dropdown = document.createElement("td");
+   //        var select = document.createElement("select");
+
+   //        // System
+   //        td_name.innerHTML = key;
+   //        select.id = key + "_select";
+
+   //        mytable.append(table_body);
+   //        table_body.append(tr);
+   //        tr.append(td_name);
+   //        tr.append(td_dropdown);
+   //        td_dropdown.append(select);
+
+   //        for (let i = 0; i < data.System.length; i++) {
+   //           var option = document.createElement("option");
+   //           option.value = data.System[i].Abk;
+   //           option.innerHTML = data.System[i].Name;
+
+   //           // console.log(data.System[i].Abk);
+   //           // console.log(data.System[i].Name);
+   //           select.appendChild(option);
+   //        }
+   //     }
+
+   // document.getElementById("typ_table").append(mytable);
 }
 
-document.getElementById("typ_table").append(mytable);
+// Read local File
+
+var dd = new dropdown("TypSchluessel_Feld");
+dd.init();
